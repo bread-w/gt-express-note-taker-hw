349b22e5ce1e98293676a5441f10f34d8048e3c2
Brad Williams <jb.wms89@gmail.com>
2020-07-06 20:40:05 -0400

    created api routes with an error catch, both get and post, next working to push posts to an array.

--- Develop/server.js
+++ Develop/server.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+let db = require("./db/db.json");
+
 const app = express();
-// const db =
 const PORT = process.env.PORT || 3000;
-
-
 
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(db);
+});
+
+app.post("/api/notes", (req, res) => {
+  fs.writeFile("./db/db.json", JSON.stringify(), (err) => {
+    if (err) {
+      console.log("Oops! There's been an error saving your note!");
+    }
+    res.json(db);
+  });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, (req, res) => {
